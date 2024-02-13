3c722866a6edd22e68df0f2a96dd4495afc14da1
Ryutaro <ruytaro@theseed.games>
2024-02-13 17:25:09 +0100

    test

---
+++ src/estatico/Coche.java
@@ -0,0 +1,13 @@
+package estatico;
+
+public class Coche {
+	String nombre;
+	String color;
+	int precio;
+	
+	public Coche(String nombre, String color, int precio) {
+		this.nombre=nombre;
+		this.color = color;
+		this.precio = precio;
+	}
+}
---
+++ src/estatico/Main.java
@@ -0,0 +1,17 @@
+package estatico;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Coche[] vehiculos = new Coche[4];
+		vehiculos[0]= new Coche("Opel Corsa","negro", 1800);
+		vehiculos[1]= new Coche("Citroen Xara","plata", 1200);
+		vehiculos[2]= new Coche("Volkswagen Escarabajo","azul", 1200);
+		vehiculos[3]= new Coche("Seat Leon","rojo", 1600);
+		
+	}
+	
+	
+
+}
